bfe87aa0aba155b8365cd4c06ecaf94cc84bb7f9
Subhampreet <subhampreet18@gmail.com>
2023-02-13 01:01:42 +0530

    Models Added

---
+++ Models/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonAPIApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonAPIApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
