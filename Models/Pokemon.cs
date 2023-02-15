053b69efac8dfdb2eae2acb3855de30f645051d1
Subhampreet <subhampreet18@gmail.com>
2023-02-15 22:13:23 +0530

    One-To-Many Releationships

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -6,5 +6,6 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public DateTime BirthDate { get; set; }
+        public ICollection<Review>? Reviews { get; set; }
     }
 }
